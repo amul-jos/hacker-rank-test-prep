b7d0a42b131d132c4dc5634873f5de6d4a735e11
amul-jos <98321461+amul-jos@users.noreply.github.com>
2022-01-28 16:02:44 +0530

    Update StringTokenizer.java

--- StringTokenizer.java
+++ StringTokenizer.java
@@ -6,12 +6,18 @@ public class Solution {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String s = scan.nextLine();
-        
-        String[] tokens = s.split("[!?\\._'@, ]+");
+        s = s.trim();
+        if(s.length()==0){
+           System.out.println("0");          
+        }
+        else if(s.length()<=400000){
+          String[] tokens = s.split("[!?\\._'@, ]+");
         System.out.println(tokens.length);
         for(int i=0;i<tokens.length;i++){
             System.out.println(tokens[i]);
+        }  
         }
+        
         scan.close();
     }
 }
