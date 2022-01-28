f1d52980432e3f33687cd72374230aab5c32a0bc
amul-jos <98321461+amul-jos@users.noreply.github.com>
2022-01-28 09:42:06 +0530

    Create Pangrams.java

---
+++ Pangrams.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'pangrams' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String pangrams(String s) {
+    s=s.toLowerCase();
+    boolean allLettersPresent = true;
+    for(char ch = 'a';ch<='z';ch++){
+        if(!s.contains(String.valueOf(ch))){          
+            allLettersPresent = false;
+            break;
+        }
+    }
+    if(allLettersPresent){ return "pangram";}
+    else{ return "not pangram";}
+
+    }
+
+}
+
+public class Pangrams {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        String result = Result.pangrams(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
